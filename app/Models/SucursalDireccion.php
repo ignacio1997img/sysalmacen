8a9322dc57bf816928967d33e7e1e6ab12ad3ffe
ignacio1997img+ <ignaciomolinaguzman20@gmail.com>
2022-09-29 11:18:25 -0400

    sucursal

--- app/Models/SucursalDireccion.php
+++ app/Models/SucursalDireccion.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class SucursalDireccion extends Model
 {
     use HasFactory;
-    
+
     protected $fillable = [
         'sucursal_id', 'direccionAdministrativa_id', 'status', 'deleted_at'
     ];
--- database/migrations/2022_09_29_111546_create_sucursal_direccions_table.php
+++ database/migrations/2022_09_29_111546_create_sucursal_direccions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSucursalDirecionsTable extends Migration
+class CreateSucursalDireccionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -30,6 +30,6 @@ class CreateSucursalDirecionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('sucursal_direcions');
+        Schema::dropIfExists('sucursal_direccions');
     }
 }
