12a8134a1f21ffe63e86051d4041ce2f57892374
ignacio1997img+ <ignaciomolinaguzman20@gmail.com>
2022-10-21 16:46:00 -0400

    login

--- resources/views/vendor/voyager/dashboard/sidebar.blade.php
+++ resources/views/vendor/voyager/dashboard/sidebar.blade.php
@@ -5,11 +5,11 @@
                 <a class="navbar-brand" href="{{ route('voyager.dashboard') }}">
                     <div class="logo-icon-container">
                         <?php $admin_logo_img = Voyager::setting('admin.icon_image', ''); ?>
-                        @if($admin_logo_img == '')
+                        {{-- @if($admin_logo_img == '')
                             <img src="{{ asset('images/icon-sidebar.png') }}" alt="Logo Icon">
                         @else
                             <img src="{{ Voyager::image($admin_logo_img) }}" alt="Logo Icon">
-                        @endif
+                        @endif --}}
                         <img src="{{ asset('images/icon-sidebar.png') }}" alt="Logo Icon">
 
                     </div>
