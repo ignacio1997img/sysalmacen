3e9cb3765cf948fc6951f8e8c21d612f147f5bc5
ignacio1997img+ <ignaciomolinaguzman20@gmail.com>
2023-01-06 09:52:41 -0400

    inventario de cierre de gestion individual

--- app/Http/Controllers/IncomeController.php
+++ app/Http/Controllers/IncomeController.php
@@ -39,7 +39,7 @@ class IncomeController extends Controller
 
         $sucursal = SucursalUser::where('user_id', Auth::user()->id)->where('condicion', 1)->where('deleted_at', null)->first();
         
-        return $sucursal;
+        // return $sucursal;
         if(!$sucursal && !auth()->user()->hasRole('admin'))
         {
             return view('errors.error');
@@ -47,7 +47,7 @@ class IncomeController extends Controller
 
 
         $user =Auth::user();
-        $gestion = InventarioAlmacen::where('status', 1)->where('sucursal_id', $sucursal->id)->where('deleted_at', null)->first();//para ver si hay gestion activa o cerrada
+        $gestion = InventarioAlmacen::where('status', 1)->where('sucursal_id', $sucursal->sucursal_id)->where('deleted_at', null)->first();//para ver si hay gestion activa o cerrada
 
         return $gestion;
         
